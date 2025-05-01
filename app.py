36085ecbde46c22ac6221419e3fdf45ba8872a70
Dan <danielkksilva1@gmail.com>
2025-05-01 13:46:14 -0400

    feat: add automtic response

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+from services.waha import Waha
+
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
     print(f'EVENTO RECEBIDO {data}')
+    waha = Waha()
+
+    chat_id = data['payload']['from']
+
+    waha.send_message(
+        chat_id=chat_id,
+        message='Resposta automatica'
+    )
+
 
     return jsonify({'status': 'success'}), 200
 
