d3915c054a8217748a35b2ad936d1de7a2057baf
Dan <danielkksilva1@gmail.com>
2025-05-04 13:48:36 -0400

    feat: preparing rag context

--- app.py
+++ app.py
@@ -10,26 +10,33 @@ from services.waha import Waha
 @app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
+
     print(f'EVENTO RECEBIDO {data}')
+    
+    chat_id = data['payload']['from']
+    event = data['event']
+    received_message = data['payload']['body'];
+
+    is_group = '@g.us' in chat_id
+    is_status = 'status@broadcast' in chat_id
+    is_event = 'session.status' in event
+
+    if is_group or is_status or is_event:
+        return jsonify({'status': 'success', 'message':'Mensagem de grupo/status ignorada'}), 200
+    
     waha = Waha()
     ai_bot = AiBot()
 
-    event = data.get('event', {})
+    waha.start_typing(chat_id)
+    time.sleep(3)
 
-    if event and event != 'session.status':
+    response = ai_bot.invoke(question=received_message)
+    waha.send_message(
+        chat_id=chat_id,
+        message=response
+    )
 
-        chat_id = data['payload']['from']
-        received_message = data['payload']['body'];
-        waha.start_typing(chat_id)
-        time.sleep(3)
-
-        response = ai_bot.invoke(question=received_message)
-        waha.send_message(
-            chat_id=chat_id,
-            message=response
-        )
-
-        waha.stop_typing(chat_id)
+    waha.stop_typing(chat_id)
 
 
 
